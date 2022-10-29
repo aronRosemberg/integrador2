75fcf02e05175507966e552b0d463a5f2e36a4bf
aron <aronrosemberg79@gmail.com>
2022-10-28 21:40:33 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,29 @@
+const categories = [{
+    name: 'carrerasCortas',
+    img: './img/persona running1.webp'
+},
+{
+    name: 'carrerasMedias',
+    img: "./img/persona running2.jpg"
+},
+{
+    name: 'carrerasLargas',
+    img: './img/pernosa running 3.jpg'
+},
+
+];
+
+const products = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+        
+    }
+]
